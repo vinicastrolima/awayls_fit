a5f2ec1b7beff41e4e7b3352abb74ba9dc62da5d
Vinícius Castro <vinicastrolimavcl@gmail.com>
2024-04-01 12:03:05 -0300

    Feat: Migration para Product, Model e Controller

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
+
 
 
 
@@ -31,3 +33,6 @@ Route::post('/users', [UserController::class, 'store']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:api')->get('/refresh', [AuthController::class, 'refresh']);
 Route::middleware('auth:api')->post('/logout', [AuthController::class, 'logout']);
+
+//Products
+Route::apiResource('/products', ProductController::class);
